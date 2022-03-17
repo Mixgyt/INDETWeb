af0ba0277624d0db7dd6fdd32f2f84a2096e4661
Cesar <44759176+Mixgyt@users.noreply.github.com>
2022-03-17 17:57:12 -0600

    added most compatibility with movil screen

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     $(window).scroll(function() {
         if( $(this).scrollTop() > 0 ){
             $('header').addClass('header1');
@@ -6,4 +6,4 @@ $(document).ready(function() {
             $('header').removeClass('header1');
         }
     })
-})
+})*/
